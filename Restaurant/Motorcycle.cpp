9f27e989f0e85e6581316086e3ce4f0a015af3cd
onecalled3bkr <44498156+onecalled3bkr@users.noreply.github.com>
2019-04-12 13:06:40 +0200

    Motorcycle files are now where they belong

--- Restaurant/Motorcycle.cpp
+++ Restaurant/Motorcycle.cpp
@@ -11,6 +11,8 @@ Motorcycle::Motorcycle(int inID, MOTOR_TYPE inType, int inSpeed, REGION inRegion
 	type = inType;
 	speed = inSpeed;
 	region = inRegion;
+	startTime=-1;
+	status=IDLE;
 }
 
 bool Motorcycle::operator>(Motorcycle & right)
@@ -22,3 +24,38 @@ bool Motorcycle::operator>(Motorcycle & right)
 Motorcycle::~Motorcycle()
 {
 }
+
+void Motorcycle::setStatus(STATUS s)
+{
+	status=s;
+}
+
+void Motorcycle::setStartTime(int time)
+	{
+		startTime=time;
+	}
+
+MOTOR_TYPE Motorcycle::getType() const
+{
+	return type;
+}
+
+	int Motorcycle::getSpeed() const
+	{
+		return speed;
+	}
+
+	REGION Motorcycle::getRegion() const
+	{
+		return region;
+	}
+
+	STATUS Motorcycle::getStatus() const
+	{
+		return status;
+	}
+
+	int Motorcycle::getStartTime() const
+	{
+       return startTime;
+	}
--- Restaurant/Motorcycle.h
+++ Restaurant/Motorcycle.h
@@ -12,12 +12,20 @@ class Motorcycle
 	int speed;		//meters it can move in one clock tick (in one timestep)
 	REGION	region;	//region of the motorcycle
 	STATUS	status;	//idle or in-service
-
+	int startTime; //the time which the motorcycle starts service 
 public:
 	Motorcycle();
 	Motorcycle(int inID, MOTOR_TYPE inType, int inSpeed, REGION inRegion);
 	bool operator>(Motorcycle& right);
 	virtual ~Motorcycle();
+	void setStatus(STATUS s);
+	void setStartTime(int time);
+	MOTOR_TYPE getType() const;
+	int getSpeed() const;
+	REGION getRegion() const;
+	STATUS getStatus() const;
+	int getStartTime() const;
+	
 };
 
 #endif
