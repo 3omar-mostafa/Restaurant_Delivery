85ada73af22d7ccbf37c159b29c49f449c273aa4
onecalled3bkr <boudy999@gmail.com>
2019-05-07 16:06:03 +0200

    Test Cases

--- Test_Cases/generate.py
+++ Test_Cases/generate.py
@@ -8,9 +8,9 @@ def write(line):
 
 #orderTypeList = ['N', 'N', 'N']
 #orderTypeList = ['N', 'F', 'N']
-#orderTypeList = ['N', 'N', 'V']
+orderTypeList = ['N', 'N', 'V']
 #orderTypeList = ['N', 'F', 'V']
-orderTypeList = ['V', 'F', 'V']
+#orderTypeList = ['V', 'F', 'V']
 regionList = ['A', 'B', 'C', 'D']
 
 name, num, motors = input("<Filename> <Number of Arrival Events> <Number of Motors>\n").split()
@@ -20,11 +20,11 @@ motors = int(motors)
 f = open(name, "w")
 
 
-# for reg in range(4):
-#     write("{} 0 0".format(motors))
-#     for i in range(1, motors + 1):
-#         write("{} {}".format(motors * reg + i, 4 * i))
-#     write("")
+for reg in range(4):
+    write("{} 0 0".format(motors))
+    for i in range(1, motors + 1):
+        write("{} {}".format(motors * reg + i, 4 * i))
+    write("")
 
 # for reg in range(4):
 #     write("{} {} 0".format(motors - 1, 1))
@@ -32,11 +32,11 @@ f = open(name, "w")
 #         write("{} {}".format(motors * reg + i, 4 * i))
 #     write("")
 
-for reg in range(4):
-    write("{} {} {}".format(1, 1, motors - 2))
-    for i in range(1, motors + 1):
-        write("{} {}".format(motors * reg + i, 4 * i))
-    write("")
+# for reg in range(4):
+#     write("{} {} {}".format(1, 1, motors - 2))
+#     for i in range(1, motors + 1):
+#         write("{} {}".format(motors * reg + i, 4 * i))
+#     write("")
 
 write("10\n\n{}".format(num))
 eventList = []
